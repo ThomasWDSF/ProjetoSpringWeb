872e607462b84b092fe68cc456f4b3384ecad9c8
ThomasWilliam <125490279+ThomasWDSF@users.noreply.github.com>
2023-03-27 15:44:27 -0300

    Atualização 1.1

---
+++ src/main/java/com/web/tornese/SpringWeb/repositorio/AdministradoresRepo.java
@@ -0,0 +1,9 @@
+package com.web.tornese.SpringWeb.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.web.tornese.SpringWeb.models.Administrador;
+
+public interface AdministradoresRepo extends CrudRepository<Administrador, Integer>{
+    
+}
